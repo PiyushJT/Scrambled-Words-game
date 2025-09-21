63653b01de494fd327534adc4ef309977ef3cb38
PiyushJT <piyushthummar7@gmail.com>
2025-09-22 00:58:54 +0530

    Initial Commit

---
+++ Scrambled Words Game/Scrambled_Words_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrambled_Words_GameApp.swift
+//  Scrambled Words Game
+//
+//  Created by Piyush Thummar on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Scrambled_Words_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
